397305d8ce9d29ef73f7df814b2162ef7a8ecce9
XDXD5856 <XDXD5856@126.com>
2025-04-21 21:01:54 +0800

    Try 03

--- push.bat
+++ push.bat
@@ -1,10 +1,10 @@
-@echo off
-set /p msg=请输入这次的提交说明：
 
+@echo off
+set /p msg=Any comment? :
 git add .
 git commit -m "%msg%"
 git push origin main
 
 echo.
-echo ✅ 已成功上传到 GitHub！
+echo Successfully push to GitHub !
 pause
